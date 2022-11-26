afda2bceef95f7c9a791301d07499b1b237220bf
Renaud DIDIER <rdidier@adequasys.com>
2022-11-26 09:08:49 +0100

    have a connexion screen (To rework with a fixed form)

--- assets/authentification/pages/authenticate.tsx
+++ assets/authentification/pages/authenticate.tsx
@@ -1,9 +1,45 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import AthLayout from "../../core/components/layout";
+import {Col, Form, Input, Row, Button} from "antd";
 
 document.addEventListener("DOMContentLoaded", function() {
     const container = document.getElementById('root');
     const root = createRoot(container!);
-    root.render( <AthLayout>coucou</AthLayout>);
+    root.render(
+        <Row justify="space-around" align="middle" style={{height:'100%', backgroundColor:'brown'}}>
+            <Col flex={3}></Col>
+            <Col flex="400px" style={{
+                height: '200px',
+                backgroundColor: 'white',
+                borderRadius: '40px',
+            }}>
+                <Form
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    style={{padding: '33px'}}
+                >
+                    <Form.Item
+                        label="Identifiant"
+                        name="username"
+                        rules={[{required: true, message: 'Renseignez votre identifiant'}]}
+                    >
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item
+                        label="Mot de passe"
+                        name="password"
+                        rules={[{required: true, message: 'Renseignez votre mot de passe'}]}
+                    >
+                        <Input.Password/>
+                    </Form.Item>
+                    <Form.Item wrapperCol={{offset: 8, span: 16}}>
+                        <Button type="primary" htmlType="submit">
+                            Connexion
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Col>
+            <Col flex={2}></Col>
+        </Row>
+    );
 })
